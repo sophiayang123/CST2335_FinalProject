6be6d0b0ec960f00b0d57623084d3518a27f138e
shuting yang <sophia12332111@gmail.com>
2019-10-30 21:08:33 -0400

    Revert "initial commit"
    This reverts commit 1797ee707c4810e3dc39ec9890b90186612ef83e.

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener((view)-> showNormalDialog());
-    }
-
-    private void showNormalDialog() {
-        final AlertDialog.Builder normalDialog = new AlertDialog.Builder(MainActivity.this);
-        normalDialog.setIcon()
     }
 }
